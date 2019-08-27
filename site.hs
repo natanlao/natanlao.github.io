a5c719f213b6f2a1bd64b8bd76e4fb82bd3cdb9f
Natan Lao <natanlao@users.noreply.github.com>
2019-08-26 19:13:55 -0700

    Use Hakyll static site generator

---
+++ site.hs
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "static/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    match (fromList ["favicon.png", "LaoNatanResume.pdf"]) $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "index.markdown" $ do
+        route $ setExtension "html"
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= renderPandoc
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
